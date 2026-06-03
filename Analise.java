31baf4339c061059d62a96caa7baf5e6a32cb0dd
diego <diegoajardo0@gmail.com>
2026-06-02 21:43:13 -0300

    feat: implementar construtor e execução de query

--- Analise.java
+++ Analise.java
@@ -1,4 +1,18 @@
 public class Analise {
  String query; 
     
+ public Analise(String query) {
+        this.query = query;
+    }
+
+public void executarQuery() { 
+    System.out.println("executando query: " + this.query);
 }
+
+public static void main(String[] args) {
+    Analise analise = new Analise("select * from dados");
+    analise.executarQuery();
+
+}
+}
+
